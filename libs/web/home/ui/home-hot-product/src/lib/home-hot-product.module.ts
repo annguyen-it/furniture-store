0ffeb3a424dcd5f82df0bee4e8adf42851ac80f2
annguyen <annguyen.it.utc@gmail.com>
2022-05-01 20:59:47 +0700

    fix import

--- libs/web/home/ui/home-hot-product/src/lib/home-hot-product.module.ts
+++ libs/web/home/ui/home-hot-product/src/lib/home-hot-product.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeHotProductComponent } from './home-hot-product.component';
+import { ProductCardModule } from '@furniture-store/web/shared/ui/product-card';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [HomeHotProductComponent, ],
+  imports: [CommonModule, ProductCardModule],
+  declarations: [HomeHotProductComponent],
   exports: [HomeHotProductComponent],
 })
 export class HomeHotProductModule {}
