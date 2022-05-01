9473addd51f1225f89163f303e884d6d88a2ac0f
annguyen <annguyen.it.utc@gmail.com>
2022-05-01 16:53:47 +0700

    Hoom room

--- libs/web/home/ui/home-room/src/lib/home-room.module.ts
+++ libs/web/home/ui/home-room/src/lib/home-room.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoomComponent } from './home-room.component';
+import { TuiButtonModule } from '@taiga-ui/core';
+
+const TAIGA_UI = [TuiButtonModule];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ...TAIGA_UI],
   declarations: [HomeRoomComponent],
   exports: [HomeRoomComponent],
 })
