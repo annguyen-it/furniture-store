c445040e51f192711832402bbb1d4c356752f2ee
Ha <nguyenhoangha1342001@gmail.com>
2022-05-03 22:57:46 +0700

    update

--- libs/web/home/ui/home-story/src/lib/home-story.component.ts
+++ libs/web/home/ui/home-story/src/lib/home-story.component.ts
@@ -1,9 +1,17 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { tuiButtonOptionsProvider } from '@taiga-ui/core';
 
 @Component({
   selector: 'fs-home-story',
   templateUrl: './home-story.component.html',
   styleUrls: ['./home-story.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    tuiButtonOptionsProvider({
+      appearance: 'fs-primary',
+      size: 'm',
+      shape: 'rounded',
+    }),
+  ],
 })
 export class HomeStoryComponent {}
