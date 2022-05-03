fcb31c14e1e1c48448ded8733bd0ffea74128a3c
annguyen <annguyen.it.utc@gmail.com>
2022-05-03 22:36:37 +0700

    Hot product

--- libs/web/home/ui/home-hot-product/src/lib/home-hot-product.module.ts
+++ libs/web/home/ui/home-hot-product/src/lib/home-hot-product.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeHotProductComponent } from './home-hot-product.component';
 import { ProductCardModule } from '@furniture-store/web/shared/ui/product-card';
+import { TuiTabsModule } from '@taiga-ui/kit';
+
+const TAIGA_UI = [TuiTabsModule];
 
 @NgModule({
-  imports: [CommonModule, ProductCardModule],
+  imports: [CommonModule, ProductCardModule, ...TAIGA_UI],
   declarations: [HomeHotProductComponent],
   exports: [HomeHotProductComponent],
 })
